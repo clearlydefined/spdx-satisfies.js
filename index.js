56121d23ed01809ee48ea186607f7a545316270a
Jeff McAffer <jmcaffer@microsoft.com>
2019-02-20 18:33:10 +0000

    use expansion to figure satisfaction

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var compare = require('spdx-compare')
 var parse = require('spdx-expression-parse')
 var ranges = require('spdx-ranges')
+var { uniqWith, isEqual } = require('lodash')
 
 var rangesAreCompatible = function (first, second) {
   return (
@@ -58,31 +59,6 @@ var licensesAreCompatible = function (first, second) {
   }
 }
 
-var recurseLeftAndRight = function (first, second) {
-  var firstConjunction = first.conjunction
-  if (firstConjunction === 'and') {
-    return (
-      recurse(first.left, second) &&
-      recurse(first.right, second)
-    )
-  } else if (firstConjunction === 'or') {
-    return (
-      recurse(first.left, second) ||
-      recurse(first.right, second)
-    )
-  }
-}
-
-var recurse = function (first, second) {
-  if (first.hasOwnProperty('conjunction')) {
-    return recurseLeftAndRight(first, second)
-  } else if (second.hasOwnProperty('conjunction')) {
-    return recurseLeftAndRight(second, first)
-  } else {
-    return licensesAreCompatible(first, second)
-  }
-}
-
 function normalizeGPLIdentifiers (argument) {
   var license = argument.license
   if (license) {
@@ -93,7 +69,7 @@ function normalizeGPLIdentifiers (argument) {
       argument.license = license.replace('-or-later', '')
       delete argument.plus
     }
-  } else {
+  } else if (argument.left && argument.right) {
     argument.left = normalizeGPLIdentifiers(argument.left)
     argument.right = normalizeGPLIdentifiers(argument.right)
   }
@@ -104,9 +80,48 @@ function endsWith (string, substring) {
   return string.indexOf(substring) === string.length - 1
 }
 
-module.exports = function (first, second) {
-  return recurse(
-    normalizeGPLIdentifiers(parse(first)),
-    normalizeGPLIdentifiers(parse(second))
-  )
+function licenseString(e) {
+  if (e.hasOwnProperty('noassertion')) return 'NOASSERTION'
+  if (e.license) return `${e.license}${e.plus ? '+' : ''}${e.exception ? ` WITH ${e.exception}` : ''}`
+}
+
+/**
+ * Expand an expression into an equivalent array where each entry is OR'd together and consists of the
+ * left expression obejects represnting the licenses to be AND'd together to be equivalent to the original.
+ * For example, (MIT OR ISC) AND GPL-3.0 would expand to [[GPL-3.0 AND MIT], [ISC AND MIT]]. Note that
+ * within each array of licenses, the entries are normalized (sorted) by license name.
+ * @param {*} expression
+ * @returns {[string]}
+ */
+function expand(expression) {
+  var expanded = Array.from(expandInner(expression))
+  var result = uniqWith(expanded.filter(e => Object.keys(e).length).map(e => Object.keys(e).sort()), isEqual)
+  for (var i = 0; i < result.length; i++) result[i] = result[i].map(license => expanded[i][license])
+  return result
+}
+
+function expandInner(expression) {
+  if (!expression.conjunction) return [{ [licenseString(expression)]: expression }]
+  if (expression.conjunction === 'or') return [...expandInner(expression.left), ...expandInner(expression.right)]
+  if (expression.conjunction === 'and') {
+    var left = expandInner(expression.left)
+    var right = expandInner(expression.right)
+    return left.reduce((result, l) => {
+      right.forEach(r => result.push({ ...l, ...r }))
+      return result
+    }, [])
+  }
+}
+
+function isANDCompatible(one, two) {
+  if (one.length !== two.length) return false
+  for (var i = 0; i < one.length; i++) if (!licensesAreCompatible(one[i], two[i])) return false
+  return true
+}
+
+module.exports = function(first, second, options) {
+  var parser = (options || {}).parse || parse
+  var one = expand(normalizeGPLIdentifiers(parser(first)))
+  var two = expand(normalizeGPLIdentifiers(parser(second)))
+  return one.some(o => two.some(t => isANDCompatible(o, t)))
 }
